1990f6e43a2b74bfd24bec87566d9a585b8844d5
apple <apple@apples-MacBook-Pro-3.local>
2017-09-18 18:44:07 +0500

    Authentication

--- flask-mysql-app/app.py
+++ flask-mysql-app/app.py
@@ -5,7 +5,7 @@ It also includes functions to add, view, delete and authenticate the user presen
 Database connectivity is also handled in the same file.
 """
 
-from flask import Flask, request, render_template, flash
+from flask import Flask, request, render_template, flash, session
 from flaskext.mysql import MySQL
 
 
@@ -14,7 +14,7 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
 app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+# app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'EmpData'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['SECRET_KEY'] = "random string"
@@ -27,7 +27,30 @@ def hello():
     Renders the main page of application
     :return: web_page
     """
+    if not session.get('logged_in'):
+        return render_template('login.html')
     return render_template('index.html')
+
+
+@app.route("/index")
+def index():
+    """
+    Renders the main page of application
+    :return: web_page
+    """
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    return render_template('index.html')
+
+
+@app.route("/logout")
+def logout():
+    """
+
+    :return:
+    """
+    session['logged_in'] = False
+    return hello()
 
 
 @app.route("/check_user")
@@ -37,6 +60,8 @@ def check_user():
     username and password matches to the database. and responds accordingly.
     :return: web_page
     """
+    if not session.get('logged_in'):
+        return render_template('login.html')
     return render_template('authenticate_user.html')
 
 
@@ -47,6 +72,8 @@ def user_remove():
     then deletes that user.
     :return: web_page
     """
+    if not session.get('logged_in'):
+        return render_template('login.html')
     return render_template('remove.html')
 
 
@@ -57,6 +84,8 @@ def authenticate():
     as saved in the DB and shows the response accordingly.
     :return message
     """
+    if not session.get('logged_in'):
+        return render_template('login.html')
     username = request.form['username']
     password = request.form['password']
     cursor = mysql.connect().cursor()
@@ -75,6 +104,8 @@ def remove():
     deletes that user from database.
     :return: web_page
     """
+    if not session.get('logged_in'):
+        return render_template('login.html')
     username = request.form['username']
     conn = mysql.get_db()
     cursor = conn.cursor()
@@ -94,6 +125,8 @@ def add_user():
     Renders the page which takes information from the user and adds new user to database.
     :return: web_page
     """
+    if not session.get('logged_in'):
+        return render_template('login.html')
     return render_template('new.html')
 
 
@@ -104,6 +137,8 @@ def new():
     Fetches data from the form and then enters it in to DB  after connecting with DB.
     :return: message or index page
     """
+    if not session.get('logged_in'):
+        return render_template('login.html')
     if request.method == 'POST':
         if not request.form['username'] or not request.form['password']:
             return "Some fields are missing"
@@ -128,11 +163,29 @@ def all_users():
     Gets all the user from the database and then display them on web page.
     :return: web_page having all the users in the DB.
     """
+    if not session.get('logged_in'):
+        return render_template('login.html')
     cursor = mysql.connect().cursor()
     cursor.execute("SELECT * from User")
     data = cursor.fetchall()
 
     return render_template("all_users.html", data=data)
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    cursor = mysql.connect().cursor()
+    cursor.execute("SELECT * from User where Username='" + username + "' and Password='" + password + "'")
+    data = cursor.fetchone()
+    if data is not None:
+        flash('User logged in Successfully', 'error')
+        session['logged_in'] = True
+        return render_template('index.html')
+    else:
+        flash('Credentials are not correct!')
+    return hello()
 
 
 if __name__ == "__main__":
